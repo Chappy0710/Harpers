75751d8093e8ded1fb61650fd530711d4f741778
Chappy0710 <111452188+Chappy0710@users.noreply.github.com>
2023-07-05 10:36:27 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/Harpers/HarpersApplication.java
@@ -0,0 +1,13 @@
+package com.Harpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarpersApplication.class, args);
+	}
+
+}
